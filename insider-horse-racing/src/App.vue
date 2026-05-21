c17d294d20d6f71fbb407547325d713275c6d09f
TReis2023 <hcvkbs@gmail.com>
2026-05-21 11:10:19 +0300

    added_raceStore

--- insider-horse-racing/src/App.vue
+++ insider-horse-racing/src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { useRaceStore } from '../stores/raceStore';
+
+const raceStore = useRaceStore()
 </script>
 
 <template>
