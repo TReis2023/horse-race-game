a53aed21726a31b25a5a44c4ffc1da80be71a137
TReis2023 <hcvkbs@gmail.com>
2026-05-21 10:46:03 +0300

    initial

---
+++ insider-horse-racing/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Horse Racing Game</h1>
+    <button>Generate Horses</button>
+  </main>
+
+</template>
