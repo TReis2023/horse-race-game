176814bb854f86babb41be47e7d3faf0def81963
TReis2023 <hcvkbs@gmail.com>
2026-05-23 11:34:53 +0300

    added_horse_generation

--- insider-horse-racing/src/App.vue
+++ insider-horse-racing/src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import { useRaceStore } from '../stores/raceStore';
 
-
+const raceStore = useRaceStore()
 
 </script>
 
 <template>
   <main>
     <h1>Horse Racing Game</h1>
-    <button>Generate Horses</button>
+    <button @click="raceStore.generateHorses">Generate Horses</button>
+    <ul>
+      <li v-for="horse in raceStore.horses" :key="horse.id">
+        Horse {{ horse.id }} |
+        Color: {{ horse.color.name }} |
+        Condition: {{ horse.condition }}
+      </li>
+    </ul>
   </main>
 
 </template>
