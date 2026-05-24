7f858816aba70b3f104989c038e35452ced2eca8
TReis2023 <hcvkbs@gmail.com>
2026-05-24 15:57:36 +0300

    added_tournament_playability

--- insider-horse-racing/src/App.vue
+++ insider-horse-racing/src/App.vue
@@ -13,8 +13,8 @@ const horseListOpen = ref(true)
     <h1>Horse Racing Game</h1>
     <aside class="all-horse">
       <button class="toggle-horse-list" @click="horseListOpen = !horseListOpen">◰</button>
-      <button @click="raceStore.generateHorses">Generate Horses</button>
-      <ul v-if="horseListOpen">
+      <button v-if="raceStore.horses.length === 0" @click="raceStore.generateHorses">Generate Horses</button>
+      <ul class="full-horse-list" v-if="horseListOpen">
         <li v-for="horse in raceStore.horses" :key="horse.id">
           Horse #{{ horse.id }} |
           Color: 
@@ -29,7 +29,7 @@ const horseListOpen = ref(true)
 
 
     <aside class="race-horse" v-if="raceStore.horses.length > 0">
-      <button @click="raceStore.chooseHorses">Choose Racing Horses</button>
+      <button v-if="raceStore.raceStatus === 'idle'" @click="raceStore.startTournament">Start Tournament</button>
       <ul class="race-horse-list">
         <li v-for="raceHorse in raceStore.racingHorses" :key="raceHorse.id">
           Horse #{{ raceHorse.id }} 
@@ -42,8 +42,9 @@ const horseListOpen = ref(true)
       </ul>
     </aside>
 
-    <aside class="race">
-      <button @click="raceStore.simulateRaces">Start Race</button>
+    <aside class="race-results" v-if="raceStore.racingHorses.length > 0">
+      <button v-if="raceStore.raceStatus === 'ready'" @click="raceStore.runCurrentRace">Start Race</button>
+      <button v-if="raceStore.raceStatus === 'finished'" @click="raceStore.nextRace">Next Race</button>
       <div v-for="raceResult in raceStore.raceResults">
         <h3>Race {{ raceResult.raceId }} - {{ raceResult.distance }} m</h3>
         <li v-for="horse in raceResult.raceResult" :key="horse.id">
@@ -51,6 +52,20 @@ const horseListOpen = ref(true)
         </li>
       </div>
     </aside>
+    <section class="race-board">
+      <div class="race-lane" v-for="horse in raceStore.racingHorses" :key="horse.id">
+        <div class="lane-label">
+          #{{ horse.id }}
+        </div>
+        <div class="lane-tiles" :style="{'--tile-count': raceStore.raceTiles.length}">
+          <div v-for="tile in raceStore.raceTiles" :key="tile.id" class="tile-type" :class="tile.type" >
+            {{ tile.type }}
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <button class="restart-button" v-if="raceStore.raceStatus === 'tournamentFinished'" @click="raceStore.startTournament">Restart Tournament</button>
 
   </main>
 
